651535b3afe2d007efdb3a85adfa1545e0dbb7e5
ramos flores <jramosfl@unprg.edu.com>
2019-12-20 09:19:59 -0500

    se agrego las carpetas

--- lote.bat
+++ lote.bat
@@ -1,78 +1,129 @@
-python app.py 10 20
-python app.py 20 10
-python app.py 5 12
-python app2.py 24 23
-python app2.py 6 3
-python app2.py 21 34
-python app3.py 130 20
-python app3.py 32 34
-python app3.py 13 45
-python app4.py 13 7
-python app4.py 9 2
-python app4.py 1 5
-python app5.py 54 45
-python app5.py 67 76
-python app5.py 90 4
-python app6.py 27 27
-python app6.py 19 21
-python app6.py 56 43
-python app7.py 13 7
-python app7.py 15 31
-python app7.py 10 11
-python app8.py 27 7
-python app8.py 4 40
-python app8.py 5 9
-python app9.py 54 2
-python app9.py 132 42
-python app9.py 118 99
-python app10.py 17 87
-python app10.py 51 52
-python app10.py 17 14
-python app11.py 65 20
-python app11.py 90 11
-python app11.py 10 50
-python app12.py 8 0
-python app12.py 13 55
-python app12.py 13 40
-python app13.py 5 3
-python app13.py 13 512
-python app13.py 17 88
-python app14.py 69 69
-python app14.py 23 69
-python app14.py 18 96
-python app15.py 68 78
-python app15.py 15 100
-python app15.py 13 47
-python app16.py 58 15
-python app16.py 100 45
-python app16.py 18 68
-python app17.py 98 101
-python app17.py 15 555
-python app17.py 17 654
-python app18.py 188 1
-python app18.py 80 124
-python app18.py 102 27
-python app19.py 31 35
-python app19.py 29 60
-python app19.py 181 100
-python app20.py 187 111
-python app20.py 17 24
-python app20.py 158 211
-python app21.py 55 255
-python app21.py 867 269
-python app21.py 118 289
-python app22.py 63 74
-python app22.py 171 62
-python app22.py 88 111
-python app23.py 18 20
-python app23.py 21 8
-python app23.py 638 65
-python app24.py 356 879
-python app24.py 99 2
-python app24.py 58 10
-python app25.py 63 11
-python app25.py 4 7
-python app25.py 11 2
+python ballena_custodio/app.py 10 20
+python ballena_custodio/app.py 20 10
+python ballena_custodio/app.py 5 12
+python ballena_custodio/app2.py 24 23
+python ballena_custodio/app2.py 6 3
+python ballena_custodio/app2.py 21 34
+python ballena_custodio/app3.py 130 20
+python ballena_custodio/app3.py 32 34
+python ballena_custodio/app3.py 13 45
+python ballena_custodio/app4.py 13 7
+python ballena_custodio/app4.py 9 2
+python ballena_custodio/app4.py 1 5
+python ballena_custodio/app5.py 54 45
+python ballena_custodio/app5.py 67 76
+python ballena_custodio/app5.py 90 4
+python ballena_custodio/app6.py 27 27
+python ballena_custodio/app6.py 19 21
+python ballena_custodio/app6.py 56 43
+python ballena_custodio/app7.py 13 7
+python ballena_custodio/app7.py 15 31
+python ballena_custodio/app7.py 10 11
+python ballena_custodio/app8.py 27 7
+python ballena_custodio/app8.py 4 40
+python ballena_custodio/app8.py 5 9
+python ballena_custodio/app9.py 54 2
+python ballena_custodio/app9.py 132 42
+python ballena_custodio/app9.py 118 99
+python ballena_custodio/app10.py 17 87
+python ballena_custodio/app10.py 51 52
+python ballena_custodio/app10.py 17 14
+python ballena_custodio/app11.py 65 20
+python ballena_custodio/app11.py 90 11
+python ballena_custodio/app11.py 10 50
+python ballena_custodio/app12.py 8 0
+python ballena_custodio/app12.py 13 55
+python ballena_custodio/app12.py 13 40
+python ballena_custodio/app13.py 5 3
+python ballena_custodio/app13.py 13 512
+python ballena_custodio/app13.py 17 88
+python ballena_custodio/app14.py 69 69
+python ballena_custodio/app14.py 23 69
+python ballena_custodio/app14.py 18 96
+python ballena_custodio/app15.py 68 78
+python ballena_custodio/app15.py 15 100
+python ballena_custodio/app15.py 13 47
+python ballena_custodio/app16.py 58 15
+python ballena_custodio/app16.py 100 45
+python ballena_custodio/app16.py 18 68
+python ballena_custodio/app17.py 98 101
+python ballena_custodio/app17.py 15 555
+python ballena_custodio/app17.py 17 654
+python ballena_custodio/app18.py 188 1
+python ballena_custodio/app18.py 80 124
+python ballena_custodio/app18.py 102 27
+python ballena_custodio/app19.py 31 35
+python ballena_custodio/app19.py 29 60
+python ballena_custodio/app19.py 181 100
+python ballena_custodio/app20.py 187 111
+python ballena_custodio/app20.py 17 24
+python ballena_custodio/app20.py 158 211
+python ballena_custodio/app21.py 55 255
+python ballena_custodio/app21.py 867 269
+python ballena_custodio/app21.py 118 289
+python ballena_custodio/app22.py 63 74
+python ballena_custodio/app22.py 171 62
+python ballena_custodio/app22.py 88 111
+python ballena_custodio/app23.py 18 20
+python ballena_custodio/app23.py 21 8
+python ballena_custodio/app23.py 638 65
+python ballena_custodio/app24.py 356 879
+python ballena_custodio/app24.py 99 2
+python ballena_custodio/app24.py 58 10
+python ballena_custodio/app25.py 63 11
+python ballena_custodio/app25.py 4 7
+python ballena_custodio/app25.py 11 2
+python ramos_flores/app.py 1123456
+python ramos_flores/app.py 351234
+python ramos_flores/app.py 1.4
+python ramos_flores/app2.py 24
+python ramos_flores/app2.py 35
+python ramos_flores/app2.py 0
+python ramos_flores/app3.py 2.3
+python ramos_flores/app3.py 1.5
+python ramos_flores/app3.py 2.0
+python ramos_flores/app4.py 1200
+python ramos_flores/app4.py 900
+python ramos_flores/app4.py 2500
+python ramos_flores/app5.py 23 34
+python ramos_flores/app5.py 67 76
+python ramos_flores/app5.py 90 4
+python ramos_flores/app6.py 27 27
+python ramos_flores/app6.py 19 21
+python ramos_flores/app6.py 56 43
+python ramos_flores/app7.py "juan"
+python ramos_flores/app7.py "maria"
+python ramos_flores/app7.py "jose"
+python ramos_flores/app8.py 123456789
+python ramos_flores/app8.py 942690958
+python ramos_flores/app8.py 971871863
+python ramos_flores/app9.py 12 45
+python ramos_flores/app9.py 132 42
+python ramos_flores/app9.py 118 99
+python ramos_flores/app10.py 17 87
+python ramos_flores/app10.py 51 52
+python ramos_flores/app10.py 17 14
+python ramos_flores/app11.py 65 20
+python ramos_flores/app11.py 90 11
+python ramos_flores/app11.py 10 50
+python ramos_flores/app12.py 8 0
+python ramos_flores/app12.py 13 55
+python ramos_flores/app12.py 13 40
+python ramos_flores/app13.py 5 3
+python ramos_flores/app13.py 13 512
+python ramos_flores/app13.py 17 88
+python ramos_flores/app14.py 69 69
+python ramos_flores/app14.py 23 69
+python ramos_flores/app14.py 18 96
+python ramos_flores/app15.py 68 78
+python ramos_flores/app15.py 15 100
+python ramos_flores/app15.py 13 47
+python ramos_flores/app16.py 58 15
+python ramos_flores/app16.py 100 45
+python ramos_flores/app16.py 18 68
+python ramos_flores/app17.py 98 101
+python ramos_flores/app17.py 15 555
+python ramos_flores/app17.py 17 654
 
 
 
